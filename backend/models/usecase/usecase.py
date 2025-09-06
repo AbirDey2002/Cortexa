2052fd25898c41efb75c092bd5fa499cbadfce68
AbirDey2002 <abirdey957@gmail.com>
2025-09-06 10:09:06 +0530

    usecase table created

---
+++ backend/models/usecase/usecase.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, ForeignKey, DateTime, JSON, String, Integer, Boolean, Text
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+import uuid
+
+from ..base import Base
+
+class UsecaseMetadata(Base):
+    """
+    UseCaseMetadata table to check the status of the usecase
+    """
+
+    __tablename__ = "usecase_metadata"
+
+    usecase_id = Column(UUID(as_uuid=True), primary_key = True, default = uuid.uuid4)
+    usecase_name = Column(String, nullable=False)
+    text_extraction = Column(String(50), default="Not Started")
+    requirement_generation = Column(String(50), default="Not Started")
+    scenario_generation = Column(String(50), default="Not Started")
+    test_case_generation = Column(String(50), default="Not Started")
+    test_script_generation = Column(String(50), default="Not Started")
+    is_deleted = Column(Boolean, default = False, nullable = False)
+    created_at = Column(DateTime, default = func.now(), nullable = False)
+    updated_at = Column(DateTime, default = func.now(), onupdate = func.now(), nullable = False)
+    email = Column(String, nullable = False)
